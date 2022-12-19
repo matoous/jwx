38f64dcbd9516086d859f1eb6ce5902ccd469926
Matouš Dzivjak <matousdzivjak@gmail.com>
2022-12-19 01:02:20 +0100

    basic rsa implementation

--- src/jwk.rs
+++ src/jwk.rs
@@ -1,53 +1,216 @@
+use base64ct::{Base64UrlUnpadded, Encoding};
+use rsa::{BigUint, PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
 use serde::{Deserialize, Serialize};
 
-use crate::{error::Error, jwt::Jwt};
+use crate::{
+    err,
+    error::{Error, Type},
+};
 
-/// JSON Web Key as described in [RFC7517](https://www.rfc-editor.org/rfc/rfc7517).
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Jwk {
+pub trait Verifier {
+    fn verify(&self, message: &[u8], signature: &[u8]) -> Result<(), Error>;
+}
+
+pub trait Signer {
+    fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error>;
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct RsaPublic {
     #[serde(default)]
     pub e: String,
-    pub kty: String,
-    pub alg: Option<String>,
     #[serde(default)]
     pub n: String,
-    pub kid: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Jwks {
-    keys: Vec<Jwk>,
-    url: String,
+impl Verifier for RsaPublic {
+    fn verify(&self, message: &[u8], signature: &[u8]) -> Result<(), Error> {
+        let pkc = RsaPublicKey::new(
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.n.as_str()).unwrap()),
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.e.as_str()).unwrap()),
+        )
+        .unwrap();
+        pkc.verify(PaddingScheme::new_pkcs1v15_sign_raw(), message, signature)
+            .or(Err(err!(
+                Certificate,
+                "Signature does not match certificate"
+            )))
+    }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct JwksResponse {
-    keys: Vec<Jwk>,
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct RsaPrivate {
+    #[serde(default)]
+    pub e: String,
+    #[serde(default)]
+    pub n: String,
+    #[serde(default)]
+    pub p: String,
+    #[serde(default)]
+    pub q: String,
+    #[serde(default)]
+    pub d: String,
+
+    pub qi: Option<String>,
+    pub dp: Option<String>,
+    pub dq: Option<String>,
 }
 
-impl Jwks {
-    pub fn new(url: String) -> Self {
-        Self {
-            url,
-            keys: Vec::new(),
+impl Verifier for RsaPrivate {
+    fn verify(&self, message: &[u8], signature: &[u8]) -> Result<(), Error> {
+        let pkc = RsaPrivateKey::from_components(
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.n.as_str()).unwrap()),
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.e.as_str()).unwrap()),
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.d.as_str()).unwrap()),
+            vec![
+                BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.p.as_str()).unwrap()),
+                BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.q.as_str()).unwrap()),
+            ],
+        )
+        .unwrap();
+        pkc.verify(PaddingScheme::new_pkcs1v15_sign_raw(), message, signature)
+            .or(Err(err!(
+                Certificate,
+                "Signature does not match certificate"
+            )))
+    }
+}
+
+impl Signer for RsaPrivate {
+    fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        let pkc = RsaPrivateKey::from_components(
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.n.as_str()).unwrap()),
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.e.as_str()).unwrap()),
+            BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.d.as_str()).unwrap()),
+            vec![
+                BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.p.as_str()).unwrap()),
+                BigUint::from_bytes_be(&Base64UrlUnpadded::decode_vec(self.q.as_str()).unwrap()),
+            ],
+        )
+        .unwrap();
+        pkc.sign(PaddingScheme::new_pkcs1v15_sign_raw(), message)
+            .map_err(|_| err!(Internal, "Sign message"))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(untagged)]
+enum Key {
+    RSAPrivate(RsaPrivate),
+    RSAPublic(RsaPublic),
+}
+
+impl Verifier for Key {
+    fn verify(&self, message: &[u8], signature: &[u8]) -> Result<(), Error> {
+        match self {
+            Key::RSAPrivate(key) => key.verify(message, signature),
+            Key::RSAPublic(key) => key.verify(message, signature),
+        }
+    }
+}
+
+impl Signer for Key {
+    fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        match self {
+            Key::RSAPrivate(key) => key.sign(message),
+            _ => Err(err!(Invalid, "Key doesn't support signing")),
+        }
+    }
+}
+
+/// JSON Web Key as described in [RFC7517](https://www.rfc-editor.org/rfc/rfc7517).
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Jwk {
+    pub kty: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub kid: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub key_ops: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub alg: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub x5u: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub x5c: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub x5t: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none", rename = "x5t#s256")]
+    pub x5t_s256: Option<String>,
+
+    #[serde(flatten)]
+    key: Key,
+}
+
+impl Jwk {
+    /// Returns the alg of this [`Jwk`].
+    pub fn alg(&self) -> String {
+        match self.key {
+            Key::RSAPrivate(_) => "RS256".into(),
+            Key::RSAPublic(_) => "RS256".into(),
         }
     }
 
-    #[cfg(feature = "reqwest")]
-    pub async fn refresh(&mut self) {
-        let body: JwksResponse = reqwest::get(&self.url).await.unwrap().json().await.unwrap();
-        self.keys = body.keys
+    pub fn parse(key: &str) -> Result<Self, Error> {
+        let jwk: Jwk =
+            serde_json::from_str(key).or(Err(err!(Invalid, "Failed to decode segment")))?;
+        Ok(jwk)
     }
 
-    pub fn decode<T>(&self, token: &str) -> Result<Jwt<T>, Error> {
-        Ok(Jwt {
-            header: todo!(),
-            payload: todo!(),
-            signature: todo!(),
-        })
+    pub fn verify(&self, message: &[u8], signature: &[u8]) -> Result<(), Error> {
+        self.key.verify(message, signature)
     }
 
-    pub fn verify<T>(&self, token: &Jwt<T>) -> Result<(), Error> {
-        Ok(())
+    pub fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        self.key.sign(message)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+    struct Claims {
+        sub: String,
+        name: String,
+        iat: i64,
+    }
+
+    #[test]
+    fn decode() {
+        let jwk = Jwk::parse(include_str!("rs256_2048_private_key.json")).unwrap();
+
+        let want_jwk = Jwk {
+            kty: "RSA".into(),
+            alg: Some("RS256".into()),
+            kid: Some("test".into()),
+            key_ops: None,
+            x5u: None,
+            x5c: None,
+            x5t: None,
+            x5t_s256: None,
+            key: Key::RSAPrivate(RsaPrivate {
+                p: "z0efhRpEUlBIlQyT2xhYrXhZHIDZs2oxOM1MpRcwdOPW1qo4fG7JrbIQ2kQepLY6zK-SssBw4KdcUhG_OuDcx-uIr6LUf0VHp0Af1ieyiceXexuBQw8URzyVCg9e8kFICHshyz6dVqS5Y1OM8kIS5l1WkYlSx7NFU0K-jo-CSK0".into(),
+                q: "uW0XqavXEr2uCpMF1Nh9SgjkaRbLhCd8x_RZpTRDpf0BqUaUNbrtF1udK6weuVh6xgLKUoE1SdjUHs5AvQmVd14aKDeYu19AQJgfnn4Y6hB2zkwYp5jCuV1PJKteC-p7XJERO8ABQNURe-PRBpfHKb4Ohp7qvW0oeQ0DZfF6K7E".into(),
+                d: "hCDlcedDhDWv9tvGBOmRPLCL7zJMckfn0f93-ZCTa5sY-FHz4Ot62Y_SLxOJjrnaGRcJqAqZqvJVXSwRzn-Vvvvgnpp3ZYCebiiyGOfV7_1E5Mdo6fNmZ1vAWfGfTghL85Td3VnryU0W1eo0gWvEx2vcSnam7I6tLmPTv4fg_7x8Uw5DeIXiq-qd8sJmBOmOXaymdRTGHxC5U-KfxXbz61-i0F099SvvSBOhY6joGlBqoxHnGlq94bjcCOwSG-cKf1gJu7mWr6EJZYHqI271S-Xn_PolHH0QzFNszQm9fMD0eQF7tJv6gchPupa2Wd5nsLsHV11hfbxc6sVmV3oLAQ".into(),
+                qi: Some( "F1QMnwPd4nEKPQdwMIVs9dmD03FPQKaC2yUx_SD2BN5hLNmMy89jwa7BcwDum5ZyN22wT6JOEc7FC-tA3-0j88VvIyihdgjFJWtpUpbvUq_1ehVwh3gc17YJm27xBYKwlFmpQLVWG4wg1h52mXlZR_9L6cNf6H4CTDFft26RxUc".into()),
+                dp: Some("CAO28UiQt7YO-GRiGyiX1S1AFNAOmtdSS-X0PrXk08AzgF1Yjcci2Sp3aFkV7jx1jZCEVZEHTEhsU2gIQtiK8Nf0kwXyvXEKUjcyg-9JAfbLrqDjoJomqJJ5GMh7XVaU2G8aYWdsYftAh8ylOIDBhlK5lCsBHmOaHJwKDi0SVok".into()),
+                dq: Some( "hrl15OifBtXcW4CBTynQtncJhjVyv111c07dx4PW1waiK1zFmNhtJXiCFNYlKKPZ6H7kg9evYS1yycMwFGmfOLCdrrTeet11MLmW17Bk58P4nmF51GPQr5_VPh5o4Z2H7jTU4aXbA0EMSAi5ueGTaofVxAg5JFLogjNrUamHC7E".into()),
+                e: "AQAB".into(),
+                n: "liMW7uxnzq8KejzQA1YC-Zk9lrV3NI3wB49pIMtzlOYwDvZOl_BbfigSCJU-8wBONAZ5is3-Ww_kOuE6KCqhGL0wSPvs5Wv7TrN_ZQNZtkM9WbJC3nIXTlLycXWFh2kh3_B0H5D4Jiz9eXZO2G1AljRkTf18K6Ep-dyJSqM8YYBxQBlE2tmhCWf-S7Zq0exwzJXeOtJ8tCvY-L25dIOBEJ7lh_FQ05iSVE1AL_PYeGKuo8oYXHvt8VUFznD4d1B9NSipmiKZuQAbbrH4Oyq-TPb0_twq2WtvN4iBCmnOosgRzmMpm2yuJ-d2kTcF8ELbJFZgVtlD1wpnO3BumrtOnQ".into(),
+            }),
+        };
+        assert_eq!(jwk, want_jwk);
+    }
+
+    #[test]
+    fn rsa() {
+        let key = Jwk::parse(include_str!("rs256_2048_private_key.json")).unwrap();
+
+        let message = "1234567890";
+        let signature = key.sign(message.as_bytes());
+        assert!(signature.is_ok());
+        let verify = key.verify(message.as_bytes(), &signature.unwrap());
+        assert!(verify.is_ok());
     }
 }
--- src/jwt.rs
+++ src/jwt.rs
@@ -1,28 +1,57 @@
+use std::marker::PhantomData;
+
+use base64ct::{Base64UrlUnpadded, Encoding};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 use crate::{
     err,
     error::{Error, Type},
+    jwk,
 };
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Header {
     alg: String,
     typ: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cty: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     enc: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    kid: Option<String>,
 }
 
 /// JSON Web Tokens as described in [RFC7519](https://tools.ietf.org/html/rfc7519).
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Jwt<T> {
-    pub header: Header,
+    pub header: Option<Header>,
     pub payload: T,
-    pub signature: String,
+    pub signature: Option<String>,
 }
 
-impl<T: DeserializeOwned> Jwt<T> {
-    fn decode(token: &str) -> Result<Self, Error> {
-        let raw_segments: Vec<&str> = token.split(".").collect();
+struct Parser<'a, T: DeserializeOwned + Serialize> {
+    token: &'a str,
+    verification_key: Option<&'a jwk::Jwk>,
+
+    phantom: PhantomData<&'a T>,
+}
+
+impl<'a, T: DeserializeOwned + Serialize> Parser<'a, T> {
+    fn new(token: &'a str) -> Self {
+        Self {
+            token,
+            verification_key: None,
+            phantom: PhantomData,
+        }
+    }
+
+    fn with_verification_key(mut self, verifier: &'a jwk::Jwk) -> Self {
+        self.verification_key = Some(verifier);
+        self
+    }
+
+    fn parse(self) -> Result<Jwt<T>, Error> {
+        let raw_segments: Vec<&str> = self.token.split(".").collect();
         if raw_segments.len() != 3 {
             return Err(err!(Invalid, "JWT does not have 3 segments"));
         }
@@ -37,27 +66,62 @@ impl<T: DeserializeOwned> Jwt<T> {
             .or(Err(err!(Invalid, "Failed to decode payload")))?;
 
         Ok(Jwt {
-            header,
+            header: Some(header),
             payload,
-            signature,
+            signature: Some(signature),
         })
     }
 }
 
+impl<T: DeserializeOwned + Serialize> Jwt<T> {
+    fn new(payload: T) -> Jwt<T> {
+        Jwt {
+            header: None,
+            payload,
+            signature: None,
+        }
+    }
+
+    fn from<'a>(token: &'a str) -> Parser<'a, T> {
+        Parser::new(token)
+    }
+
+    fn sign(&self, jwk: &jwk::Jwk) -> Result<String, Error> {
+        let mut token = encode_segment(&Header {
+            alg: jwk.alg(),
+            typ: "JWT".into(),
+            kid: jwk.kid.clone(),
+            enc: None,
+            cty: None,
+        })?;
+        token.push('.');
+        token.push_str(&encode_segment(&self.payload)?);
+
+        let signature = jwk.sign(token.as_bytes())?;
+        let payload_segment = Base64UrlUnpadded::encode_string(&signature);
+
+        token.push('.');
+        token.push_str(&payload_segment);
+
+        Ok(token)
+    }
+}
+
 fn decode_segment<T: DeserializeOwned>(segment: &str) -> Result<T, Error> {
-    let raw = base64::decode_engine(
-        segment,
-        &base64::engine::fast_portable::FastPortable::from(
-            &base64::alphabet::URL_SAFE,
-            base64::engine::fast_portable::NO_PAD,
-        ),
-    )
-    .or(Err(err!(Invalid, "Failed to decode segment")))?;
+    let raw = Base64UrlUnpadded::decode_vec(segment)
+        .or(Err(err!(Invalid, "Failed to decode segment")))?;
+
     let slice = String::from_utf8_lossy(&raw);
     let decoded: T =
         serde_json::from_str(&slice).or(Err(err!(Invalid, "Failed to decode segment")))?;
 
     Ok(decoded)
+}
+
+#[inline]
+fn encode_segment<T: Serialize>(segment: &T) -> Result<String, Error> {
+    let encoded = serde_json::to_vec(segment).or(Err(err!(Invalid, "Failed to decode segment")))?;
+    Ok(Base64UrlUnpadded::encode_string(&encoded))
 }
 
 #[cfg(test)]
@@ -73,13 +137,15 @@ mod tests {
 
     #[test]
     fn decode() {
-        let jwt: Jwt<Claims> = Jwt::decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c").unwrap();
+        let jwt: Jwt<Claims> = Jwt::from("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c").parse().unwrap();
         assert_eq!(
-            jwt.header,
+            jwt.header.unwrap(),
             Header {
                 alg: "HS256".into(),
                 typ: "JWT".into(),
                 enc: None,
+                kid: None,
+                cty: None,
             }
         );
         assert_eq!(
@@ -90,6 +156,55 @@ mod tests {
                 iat: 1516239022
             }
         );
-        assert_eq!(jwt.signature, "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+        assert_eq!(
+            jwt.signature.unwrap(),
+            "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        );
+    }
+
+    #[test]
+    fn sign() {
+        let jwk = jwk::Jwk::parse(include_str!("./rs256_2048_private_key.json")).unwrap();
+        let jwt = Jwt::new(Claims {
+            sub: "1234567890".into(),
+            name: "John Doe".into(),
+            iat: 1516239022,
+        });
+        let res = jwt.sign(&jwk);
+        assert!(res.is_ok());
+        println!("{:?}", res);
+
+        let jwt: Jwt<Claims> = Jwt::from(&res.unwrap())
+            .with_verification_key(&jwk)
+            .parse()
+            .unwrap();
+        assert_eq!(
+            jwt.header.unwrap(),
+            Header {
+                alg: "RS256".into(),
+                typ: "JWT".into(),
+                kid: Some("test".into()),
+                enc: None,
+                cty: None,
+            }
+        );
+        assert_eq!(
+            jwt.payload,
+            Claims {
+                sub: "1234567890".into(),
+                name: "John Doe".into(),
+                iat: 1516239022
+            }
+        );
+        assert_eq!(
+            jwt.signature.unwrap(),
+            "ATamiUP7uF_FWIemhEv610lFOZlyhCktRET9QiEQUuBKmL-V7O9G52I9x7J_W-oq2e_nTQHDEXNQjsXUTf9wBfku8maWkcfULRtD47ToyHG4mowThtuhTtJgwF9oQQlOAndn6zLllIf_tbL-rqWv36KdoskhBJn-RPYV495ZVkY8vNl9cf9mFLA5z2tvTVc8uJapLPP-t-l_EQwAWHGKRjFHKoeejt-_UsaXyRXrR7M_MtCz8QBgCeC4E9JeoBPfKS43ZJHhqW6TOb786gaR6H6-0iEz3SF0pHs7Fm8Qrus5yqSe4zpWbHafG2j00e4t2HSP4Eg664iy5cNREB2sGw"
+        );
+    }
+
+    #[test]
+    fn verify() {
+        let key = jwk::Jwk::parse(include_str!("rs256_2048_private_key.json")).unwrap();
+        let jwt: Jwt<Claims> = Jwt::from("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.YCSbIl71ucUlggqB4_6dErtfMq3n80LLKCbguSKp3iN8TZ_iRBW3Dw-75MlC8ooCFw7ketVxbPhkfvbGsyZkIfM1LIg4iY7mlxtFkxZUrY5mT7ymJRNJDLXAOvHpYnOckjgmjOQcGbin_LECxkqywi7BrOemEYZl5hPEJ3Wsgk-Ca4LNqk2XXaHpT-Tiz4Qqc6UDagn83bZDQrHSedq-67HoWiOQNLipaG_7si4yRNOZKry3YFkulrE7K64sT92z_uEg4WOcZXtXtwhnrNdcnlw0eWle97N_L7pxYF1DUraZvnxuiiYcqNfbub29op0-ZskCNhwM_1OLbC8axTdpTQ").with_verification_key(&key).parse().unwrap();
     }
 }
